c3e23914cbaeb20e29b3ed4b4a36d5dcbe819302
unknown <kevinaborders@gmail.com>
2021-10-12 22:08:40 -0500

    thymeleaf exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -9,18 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
-        return "events/index";
+//        model.addAttribute("events", events);
+//        return "events/index";
+          events.put("Birthday Party", "It is my nephews 5th Birthday.");
+          events.put("Christmas", "Lets exchange some gifts.");
+          events.put("Halloween", "Too old to go trick or treating.");
+          model.addAttribute("events", events);
+          return "events/index";
     }
     // lives at /events/create
     @GetMapping("create")
@@ -30,7 +36,7 @@ public class EventController {
 
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+        events.put(eventName, "No Description");
         return "redirect:";
     }
 
